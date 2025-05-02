f3fe3f864d18939864629c315e20f145497cd069
sylvia <silvia.youssef03@eng-st.cu.edu.eg>
2025-05-02 10:30:33 +0300

    sh_commit

--- Session_Management.py
+++ Session_Management.py
@@ -273,6 +273,7 @@ async def login_for_access_token(
     
     # Send data to Logstash
     # Prepare data for Logstash
+    print(user)
     user_info = {
         "username": form_data.username,
         "location": current_location,
